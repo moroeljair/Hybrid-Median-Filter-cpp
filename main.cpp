05373ecec1af445839e2e04a412425bfe76821c6
Jair Morocho <66647841+moroeljair@users.noreply.github.com>
2022-06-16 15:03:36 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,6 +6,7 @@ using namespace std;
 typedef int element;
 
 vector<vector<element>> hybridmedianfilterN(vector<vector<element>> image, int MN_vecindad=1);
+vector<vector<element>> hybridmedianfilterN2(vector<vector<element>> image, int MN_vecindad=1);
 element median(element* elements, int N);
 
 int main()
@@ -51,6 +52,74 @@ element median(element* elements, int N)
 
 
 vector<vector<element>> hybridmedianfilterN(vector<vector<element>> image, int MN_vecindad)
+{
+
+    vector<vector<element>> img_result = image;
+    //N width, M height
+    element N = image[0].size();
+    element M = image.size();
+    //tamano de los elementos de la ventana
+    int tW = (MN_vecindad*4) + 1;
+
+    element window[tW];
+    element results[3];
+
+    int x,y,posicion;
+
+   //   Move window through all elements of the image
+   #pragma omp parallel for collapse(2) shared(img_result,image,M,N,MN_vecindad) private(window,results,x,y) schedule(static)
+   for (y = MN_vecindad; y < M - MN_vecindad; ++y)
+      for (x = MN_vecindad; x < N - MN_vecindad; ++x)
+      {
+
+
+         //   Pick up cross-window elements
+         posicion=0;
+         window[0] = image[y][x];
+         for(int i=1; i<=MN_vecindad; i++){
+             window[1+posicion] = image[(y-i)][x];
+             window[2+posicion] = image[y][(x-i)];
+             window[3+posicion] = image[y][(x+i)];
+             window[4+posicion] = image[(y+i)][x];
+             posicion=i*4;
+         }
+         //   Get median
+         results[0] = median(window, tW);
+
+         //   Pick up x-window elements
+         window[0] = image[y][x];
+         posicion=0;
+         for(int i=1; i<=MN_vecindad; i++){
+             window[1+posicion] = image[(y-i)][(x-i)];
+             window[2+posicion] = image[(y-i)][(x+i)];
+             window[3+posicion] = image[(y+i)][(x-i)];
+             window[4+posicion] = image[(y+i)][(x+i)];
+             posicion=i*4;
+         }
+         //   Get median
+         results[1] = median(window, tW);
+         //   Pick up leading element
+         results[2] = image[y][x];
+         //   Get result
+         img_result[y][x] = median(results, 3);
+      }
+
+
+   return img_result;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+vector<vector<element>> hybridmedianfilterN2(vector<vector<element>> image, int MN_vecindad)
 {
 
     vector<vector<element>> img_result = image;
